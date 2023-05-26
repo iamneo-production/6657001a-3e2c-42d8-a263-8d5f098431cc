a2087e09ee015e138b20abd1db5c14641382fe92
putheti <puthetisudeepthi2002@gmail.com>
2023-05-26 05:36:23 +0000

    created admissionform component

---
+++ angularapp/src/app/components/userside/admissionform/admissionform.ts
@@ -0,0 +1,17 @@
+export interface Enroll{
+    phoneNumber1:String,
+    phoneNumber2:String,
+    emailId: String,
+    firstName: String,
+    lastName:String,
+    age:String,
+    gender: String,
+    fatherName:String,
+    motherName:String,
+    houseNo:String,
+    streetName:String,
+    areaName:String,
+    pincode: String,
+    state:String,
+    nationality:String
+}
