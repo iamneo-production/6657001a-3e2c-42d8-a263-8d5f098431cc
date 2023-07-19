2d69511229aa01a4cf4bb522bfac419dcd747f61
7095767662 <syamsundar330@gmail.com>
2023-07-19 10:07:38 +0000

    updated signup component

--- angularapp/src/app/components/auth/signup/signup.component.ts
+++ angularapp/src/app/components/auth/signup/signup.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup,FormBuilder,Validators } from '@angular/forms';
+import { SignupService } from 'src/app/services/authservice.service';
 
 @Component({
   selector: 'app-signup',
@@ -6,34 +8,67 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
-
-  constructor() { }
-
   ngOnInit(): void {
   }
   public user={
-    admin_user:'',
+    
+    userRole:'',
     email:'',
     username:'',
     mobileNumber:'',
     password:'',
     confirmPassword:'',
     };
+  userForm : FormGroup;
+  constructor(private formBuilder: FormBuilder,private signup:SignupService) {
+    this.userForm = this.formBuilder.group({
+      admin_user: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      username: ['', Validators.required],
+      mobileNumber: ['', [Validators.required,Validators.minLength(10),Validators.maxLength(10)]],
+      password: ['', [Validators.required,Validators.minLength(8)]],
+      confirmPassword: ['', Validators.required]
+    });
+  }
+
+
+
     formSubmit(){
-    if(this.user.email && this.user.admin_user && this.user.username && this.user.mobileNumber &&this.user.password && this.user.confirmPassword){
+    if(this.user.email && this.user.userRole && this.user.username && this.user.mobileNumber &&this.user.password && this.user.confirmPassword){
       if(this.user.password != this.user.confirmPassword){
 
         alert('Passwords mismatch. Both passwords must be same!!');
-      }
-      else{
-        alert('Registration successful');
-        
-      }
-         
+      }  
     }
     else{
-      alert('All fields are mandatory');
+      alert('Something Went Wrong');
     }
+    if(this.user.userRole=="admin"){
+      this.signup.addAdmin(this.user).subscribe(
+        (data)=>{
+          console.log(data)
+          alert("Admin Registration Successful")
+        },
+        (error)=>{
+          console.log(error)
+          alert("Admin Already Present")
+        }
+        );
+    }
+    else{
+      this.signup.addUser(this.user).subscribe(
+        (data)=>{
+          console.log(data)
+          alert("User Registration Successful")
+        },
+        (error)=>{
+          console.log(error)
+          alert("User already Present")
+        }
+        );
+      
+    }
+
     
   }
 
