ad5a8b2c33efbd8f5e4cc7dd3a5c43f913522131
kumarudhay121 <kumarudhay121@gmail.com>
2023-07-19 19:37:23 +0530

    implemented backend for student

---
+++ springapp/src/main/java/com/examly/springapp/model/StudentModel.java
@@ -0,0 +1,182 @@
+package com.examly.springapp.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "students")
+public class StudentModel {
+    @Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer studentId;
+	@Column(name = "first_name")
+	private String firstName;
+	@Column(name = "last_name")
+	private String lastName;
+    @Column(name = "gender")
+	private String gender;
+	@Column(name = "father_name")
+	private String fatherName;
+	@Column(name = "mother_name")
+	private String motherName;
+	@Column(name = "age")
+	private Integer age;
+	@Column(name = "mobile_no")
+	private String mobile;
+	@Column(name = "alt_mob_no")
+	private String altMobile;
+	@Column(name = "email_id")
+	private String emailId;
+	@Column(name = "house_no")
+	private String houseNo;
+	@Column(name = "street_name")
+	private String streetName;
+	@Column(name = "area_name")
+	private String areaName;
+	@Column(name = "pin_code")
+	private Integer pincode;
+	@Column(name = "nationality")
+	private String nationality;
+	@Column(name = "state")
+	private String state;
+    @Column(name = "course_name")
+	private String courseName;
+
+
+    public StudentModel() {
+    }
+
+
+    public StudentModel(Integer studentId, String firstName, String lastName, String gender, String fatherName,
+            String motherName, Integer age, String mobile, String altMobile, String emailId, String houseNo,
+            String streetName, String areaName, Integer pincode, String nationality, String state, String courseName
+             ) {
+        this.studentId = studentId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gender = gender;
+        this.fatherName = fatherName;
+        this.motherName = motherName;
+        this.age = age;
+        this.mobile = mobile;
+        this.altMobile = altMobile;
+        this.emailId = emailId;
+        this.houseNo = houseNo;
+        this.streetName = streetName;
+        this.areaName = areaName;
+        this.pincode = pincode;
+        this.nationality = nationality;
+        this.state = state;
+        this.courseName = courseName;
+    }
+
+    public Integer getStudentId() {
+        return studentId;
+    }
+    public void setStudentId(Integer studentId) {
+        this.studentId = studentId;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public String getGender() {
+        return gender;
+    }
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+    public String getFatherName() {
+        return fatherName;
+    }
+    public void setFatherName(String fatherName) {
+        this.fatherName = fatherName;
+    }
+    public String getMotherName() {
+        return motherName;
+    }
+    public void setMotherName(String motherName) {
+        this.motherName = motherName;
+    }
+    public Integer getAge() {
+        return age;
+    }
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+    public String getMobile() {
+        return mobile;
+    }
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+    public String getAltMobile() {
+        return altMobile;
+    }
+    public void setAltMobile(String altMobile) {
+        this.altMobile = altMobile;
+    }
+    public String getEmailId() {
+        return emailId;
+    }
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+    public String getHouseNo() {
+        return houseNo;
+    }
+    public void setHouseNo(String houseNo) {
+        this.houseNo = houseNo;
+    }
+    public String getStreetName() {
+        return streetName;
+    }
+    public void setStreetName(String streetName) {
+        this.streetName = streetName;
+    }
+    public String getAreaName() {
+        return areaName;
+    }
+    public void setAreaName(String areaName) {
+        this.areaName = areaName;
+    }
+    public Integer getPincode() {
+        return pincode;
+    }
+    public void setPincode(Integer pincode) {
+        this.pincode = pincode;
+    }
+    public String getNationality() {
+        return nationality;
+    }
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+    public String getState() {
+        return state;
+    }
+    public void setState(String state) {
+        this.state = state;
+    }
+    public String getCourseName() {
+        return courseName;
+    }
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+
+
+}
