7de271e47c3e1de57256c15c99bc001dafd74526
kumarudhay121 <kumarudhay121@gmail.com>
2023-07-21 12:40:57 +0530

    added institute componenet front end

--- angularapp/src/app/components/adminside/institutes/institutes.component.ts
+++ angularapp/src/app/components/adminside/institutes/institutes.component.ts
@@ -1,15 +1,59 @@
 import { Component, OnInit } from '@angular/core';
+import { Institute } from './institute';
+import { InstituteService } from 'src/app/services/institute.service';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-institutes',
   templateUrl: './institutes.component.html',
   styleUrls: ['./institutes.component.css']
 })
-export class InstitutesComponent implements OnInit {
+export class InstitutesComponent implements OnInit{
 
-  constructor() { }
+  institutes!: Institute[];
+  searchText !: string;
 
-  ngOnInit(): void {
+  constructor(private router: Router, private instituteService: InstituteService) {
+
   }
 
+  ngOnInit(): void {
+    this.getInstitutes();
+  }
+
+  getInstitutes() {
+    this.instituteService.viewInstitutes().subscribe(data => {
+      this.institutes = data;
+    });
+  }
+
+  editInstitute(instituteId: number) {
+    this.router.navigate(['admin/editInstitute', instituteId]);
+
+  }
+
+  deleteInstitute(instituteId: number) {
+    this.instituteService.deleteInstitute(instituteId).subscribe(data => {
+      this.getInstitutes();
+    });
+  }
+  
+
+  getStars(rating: number): number[] {
+    return Array(Math.floor(rating)).fill(0);
+  }
+
+  filterInstitutes(): Institute[] {
+    if (!this.searchText || this.searchText.trim() === '') {
+      return this.institutes;
+    }
+
+    const searchLowerCase = this.searchText.toLowerCase();
+    return this.institutes.filter((institute: Institute) => {
+      const nameLowerCase = institute.instituteName.toLowerCase();
+      const locationLowerCase = institute.instituteAddress.toLowerCase();
+      return nameLowerCase.includes(searchLowerCase) || locationLowerCase.includes(searchLowerCase);
+    });
+  }
 }
---
+++ angularapp/src/app/services/institute.service.ts
@@ -0,0 +1,34 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Institute } from '../components/adminside/institutes/institute';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InstituteService {
+
+  constructor(private http: HttpClient) { }
+  baseUrl: string = 'http://localhost:8080-/';
+
+  viewInstitutes(): Observable<Institute[]> {
+    return this.http.get<Institute[]>(this.baseUrl + 'admin/institute');
+  }
+
+  getInstituteById(instituteId: number): Observable<Institute> {
+    return this.http.get<Institute>(this.baseUrl + 'admin/institute/' + instituteId);
+  }
+
+  addInstitute(institute: Institute): Observable<Institute> {
+    return this.http.post<Institute>(this.baseUrl + 'admin/addInstitute', institute);
+  }
+
+  editInstitute(instituteId: number,institute: Institute): Observable<Institute> {
+    return this.http.put<Institute>(this.baseUrl + 'admin/editInstitute/' + instituteId, institute);
+  }
+
+  deleteInstitute(instituteId: number): Observable<Institute> {
+    return this.http.delete<Institute>(this.baseUrl + 'admin/deleteInstitute/' + instituteId);
+  }
+
+}
